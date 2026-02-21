2444d9ecd93cf27424eb4e85a71e1d2a058ca265
Evgeniyc <evgeniychp@i.ua>
2026-02-21 11:54:34 +0100

    Последние изменения

--- frm/config/db.php
+++ frm/config/db.php
@@ -1,14 +1,21 @@
 <?php
+// Определяем путь к локальному конфигу
+$localConfig = __DIR__ . '/db.local.php';
 
-return [
-    'class' => yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=egewe258_simplemapp',
-    'username' => 'egewe258_admin ',
-    'password' => '',
-    'charset' => 'utf8mb4',
+if (file_exists($localConfig)) {
+    // Если db.local.php есть — используем его (локально)
+    return require $localConfig;
+} else {
+    return [
+        'class' => yii\db\Connection::class,
+        'dsn' => 'mysql:host=localhost;dbname=egewe258_simplemapp',
+        'username' => 'egewe258_admin ',
+        'password' => '',
+        'charset' => 'utf8mb4',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
+}
