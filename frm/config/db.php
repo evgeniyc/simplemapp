14b3bb32994ca624aab1e44dd36a897e6e0c7a61
Evgeniyc <evgeniychp@i.ua>
2026-02-21 11:01:09 +0100

    Коррекция .git

--- frm/config/db.php
+++ frm/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => yii\db\Connection::class,
     'dsn' => 'mysql:host=localhost;dbname=egewe258_simplemapp',
     'username' => 'egewe258_admin ',
-    'password' => '251173chp',
+    'password' => '',
     'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
